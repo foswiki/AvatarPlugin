007d4cb24ac17e27a875e165529fbcee2c6b50ee
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-04-08 05:13:31 +0000

    Item10607: minimise plugin.pm by removing commented out boilerplate
    git-svn-id: http://svn.foswiki.org/trunk/AvatarPlugin@11334 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/AvatarPlugin.pm
+++ lib/Foswiki/Plugins/AvatarPlugin.pm
@@ -1,45 +1,13 @@
 package Foswiki::Plugins::AvatarPlugin;
 
-# Always use strict to enforce variable scoping
 use strict;
 use warnings;
 
 use Foswiki::Func    ();    # The plugins API
 use Foswiki::Plugins ();    # For the API version
-
-# $VERSION is referred to by Foswiki, and is the only global variable that
-# *must* exist in this package. This should always be in the format
-# $Rev$ so that Foswiki can determine the checked-in status of the
-# extension.
 our $VERSION = '$Rev$';
-
-# $RELEASE is used in the "Find More Extensions" automation in configure.
-# It is a manually maintained string used to identify functionality steps.
-# You can use any of the following formats:
-# tuple   - a sequence of integers separated by . e.g. 1.2.3. The numbers
-#           usually refer to major.minor.patch release or similar. You can
-#           use as many numbers as you like e.g. '1' or '1.2.3.4.5'.
-# isodate - a date in ISO8601 format e.g. 2009-08-07
-# date    - a date in 1 Jun 2009 format. Three letter English month names only.
-# Note: it's important that this string is exactly the same in the extension
-# topic - if you use %$RELEASE% with BuildContrib this is done automatically.
 our $RELEASE = '1.0.0';
-
-# Short description of this plugin
-# One line description, is shown in the %SYSTEMWEB%.TextFormattingRules topic:
 our $SHORTDESCRIPTION = 'embed Avatars into foswiki';
-
-# You must set $NO_PREFS_IN_TOPIC to 0 if you want your plugin to use
-# preferences set in the plugin topic. This is required for compatibility
-# with older plugins, but imposes a significant performance penalty, and
-# is not recommended. Instead, leave $NO_PREFS_IN_TOPIC at 1 and use
-# =$Foswiki::cfg= entries, or if you want the users
-# to be able to change settings, then use standard Foswiki preferences that
-# can be defined in your %USERSWEB%.SitePreferences and overridden at the web
-# and topic level.
-#
-# %SYSTEMWEB%.DevelopingPlugins has details of how to define =$Foswiki::cfg=
-# entries so they can be used with =configure=.
 our $NO_PREFS_IN_TOPIC = 1;
 
 sub initPlugin {
@@ -57,27 +25,8 @@ sub initPlugin {
     return 1;
 }
 
-# The function used to handle the %EXAMPLETAG{...}% macro
-# You would have one of these for each macro you want to process.
 sub AVATAR {
     my($session, $params, $topic, $web, $topicObject) = @_;
-#    # $session  - a reference to the Foswiki session object
-#    #             (you probably won't need it, but documented in Foswiki.pm)
-#    # $params=  - a reference to a Foswiki::Attrs object containing 
-#    #             parameters.
-#    #             This can be used as a simple hash that maps parameter names
-#    #             to values, with _DEFAULT being the name for the default
-#    #             (unnamed) parameter.
-#    # $topic    - name of the topic in the query
-#    # $web      - name of the web in the query
-#    # $topicObject - a reference to a Foswiki::Meta object containing the
-#    #             topic the macro is being rendered in (new for foswiki 1.1.x)
-#    # Return: the result of processing the macro. This will replace the
-#    # macro call in the final text.
-#
-#    # For example, %EXAMPLETAG{'hamburger' sideorder="onions"}%
-#    # $params->{_DEFAULT} will be 'hamburger'
-#    # $params->{sideorder} will be 'onions'
 
     my $linkText = '';
     my @emails = Foswiki::Func::wikinameToEmails($params->{_DEFAULT});
@@ -119,13 +68,11 @@ Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 
 Author: SvenDowideit
 
-Copyright (C) 2008-2011 Foswiki Contributors. Foswiki Contributors
-are listed in the AUTHORS file in the root of this distribution.
-NOTE: Please extend that file, not this notice.
+Copyright (C) 2011 Sven Dowideit http://fosiki.com
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
+as published by the Free Software Foundation; either version 3
 of the License, or (at your option) any later version. For
 more details read LICENSE in the root of this distribution.
 
