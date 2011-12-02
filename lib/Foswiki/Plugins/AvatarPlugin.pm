44c83d821b32e3b35a1b5b09f6aac7d8c35dc173
CrawfordCurrie <CrawfordCurrie@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-12-02 12:29:21 +0000

    Item11320: some doc cleanup
    git-svn-id: http://svn.foswiki.org/trunk/AvatarPlugin@13279 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/AvatarPlugin.pm
+++ lib/Foswiki/Plugins/AvatarPlugin.pm
@@ -5,10 +5,19 @@ use warnings;
 
 use Foswiki::Func    ();    # The plugins API
 use Foswiki::Plugins ();    # For the API version
-our $VERSION = '$Rev$';
-our $RELEASE = '1.0.0';
-our $SHORTDESCRIPTION = 'embed Avatars into foswiki';
+our $VERSION           = '$Rev$';
+our $RELEASE           = '1.0.0';
+our $SHORTDESCRIPTION  = 'Embed Avatars into Foswiki topics';
 our $NO_PREFS_IN_TOPIC = 1;
+
+=pod
+
+---++ Future development
+   2 gravatar profile popups
+   4 context aware size options (configurable default and size) - or perhaps replace plugin magic with a =AVATAR= for more control
+   5 could it be possible to over-ride the remote server with a user topic setting? allowing local foswiki customisations
+
+=cut
 
 sub initPlugin {
     my ( $topic, $web, $user, $installWeb ) = @_;
@@ -20,81 +29,97 @@ sub initPlugin {
     }
     Foswiki::Func::registerTagHandler( 'AVATAR', \&AVATAR );
 
-
     # Plugin correctly initialized
     return 1;
 }
 
 sub AVATAR {
-    my($session, $params, $topic, $web, $topicObject) = @_;
+    my ( $session, $params, $topic, $web, $topicObject ) = @_;
 
     my $size = $params->{size} || 20;
- 
 
     my $linkText = '';
-    if (not defined($Foswiki::cfg{AvatarPlugin}{UsePersonalInfoAddOn}) or (not $Foswiki::cfg{AvatarPlugin}{UsePersonalInfoAddOn})) {
-        my @emails = Foswiki::Func::wikinameToEmails($params->{_DEFAULT});
+    if ( not defined( $Foswiki::cfg{AvatarPlugin}{UsePersonalInfoAddOn} )
+        or ( not $Foswiki::cfg{AvatarPlugin}{UsePersonalInfoAddOn} ) )
+    {
+        my @emails = Foswiki::Func::wikinameToEmails( $params->{_DEFAULT} );
+
         #TODO: consider the other emails later..
         my $email = $emails[0] || $topic;
         use URI::Escape qw(uri_escape);
         use Digest::MD5 qw(md5_hex);
         my $default = $params->{default} || "mm";
-        #TODO: tmpl?
-        my $host = $Foswiki::cfg{AvatarPlugin}{AvatarServerBaseUrl} || 'http://cdn.libravatar.org/avatar/';
-        my $grav_url = $host.md5_hex(lc $email)."?d=".uri_escape($default)."&s=".$size;
 
-        $linkText = "<img src=\"$grav_url\" />".$linkText;
-    } else {
+        #TODO: tmpl?
+        my $host = $Foswiki::cfg{Plugins}{AvatarPlugin}{ServerBaseUrl}
+          || $Foswiki::cfg{AvatarPlugin}{AvatarServerBaseUrl}    # legacy
+          || 'http://cdn.libravatar.org/avatar/';
+        my $grav_url =
+            $host
+          . md5_hex( lc $email ) . "?d="
+          . uri_escape($default) . "&s="
+          . $size;
+
+        $linkText = "<img src=\"$grav_url\" />" . $linkText;
+    }
+    else {
+
         #TODO: check access permission
-        my ( $meta, $text ) = Foswiki::Func::readTopic( $Foswiki::cfg{UsersWebName},  $params->{_DEFAULT});
-        
+        my ( $meta, $text ) =
+          Foswiki::Func::readTopic( $Foswiki::cfg{UsersWebName},
+            $params->{_DEFAULT} );
+
         my $picture = $meta->get( 'FIELD', 'Picture' );
-        
+
         #TODO: all this needs to be in a tmpl, so it can be skinned.
 
-        #$linkText = "<img height=\"".$size."px\" src=\"".Foswiki::Func::getPubUrlPath().'/'.$Foswiki::cfg{UsersWebName}.'/'.$params->{_DEFAULT}.'/'.$picture->{value}."\" />".$linkText;
-        
+#$linkText = "<img height=\"".$size."px\" src=\"".Foswiki::Func::getPubUrlPath().'/'.$Foswiki::cfg{UsersWebName}.'/'.$params->{_DEFAULT}.'/'.$picture->{value}."\" />".$linkText;
+
         #pub/System/PersonalInfoAddOn/silhouette.gif
-        my $iweb = $Foswiki::cfg{SystemWebName};
+        my $iweb   = $Foswiki::cfg{SystemWebName};
         my $itopic = 'PersonalInfoAddOn';
-        my $image = 'silhouette.gif';
-        if (defined($picture) and defined($picture->{value})) {
-            $iweb = $Foswiki::cfg{UsersWebName};
+        my $image  = 'silhouette.gif';
+        if ( defined($picture) and defined( $picture->{value} ) ) {
+            $iweb   = $Foswiki::cfg{UsersWebName};
             $itopic = $params->{_DEFAULT};
-            $image = $picture->{value};
+            $image  = $picture->{value};
         }
-        
+
         #ask ImagePlugin for an image the right size..
         require Foswiki::Plugins::ImagePlugin;
-        $linkText = Foswiki::Plugins::ImagePlugin::handleIMAGE($session, {
-                                                                #_DEFAULT => Foswiki::Func::getPubUrlPath().'/'.$Foswiki::cfg{UsersWebName}.'/'.$params->{_DEFAULT}.'/'.$picture->{value},
-                                                                web => $iweb,
-                                                                topic => $itopic,
-                                                                _DEFAULT => $image,
-                                                                alt=>" ", 
-                                                                height => $size
-                                                            });
-        
-        
+        $linkText = Foswiki::Plugins::ImagePlugin::handleIMAGE(
+            $session,
+            {
+
+#_DEFAULT => Foswiki::Func::getPubUrlPath().'/'.$Foswiki::cfg{UsersWebName}.'/'.$params->{_DEFAULT}.'/'.$picture->{value},
+                web      => $iweb,
+                topic    => $itopic,
+                _DEFAULT => $image,
+                alt      => " ",
+                height   => $size
+            }
+        );
     }
     return $linkText;
 }
 
 sub renderWikiWordHandler {
-    my( $linkText, $hasExplicitLinkLabel, $web, $topic ) = @_;
-    
-    if (#test if its a baseusermapper user
-        ($web eq $Foswiki::cfg{UsersWebName}) and
-        ($linkText eq $topic) and
-        Foswiki::Func::wikiToUserName($topic) and
-        (not Foswiki::Func::isGroupMember('BaseGroup', $topic))
-            ) {
-            $linkText = AVATAR($Foswiki::Plugins::SESSION, {_DEFAULT=>$topic}).$linkText;
-        }
-    
+    my ( $linkText, $hasExplicitLinkLabel, $web, $topic ) = @_;
+
+    if (    #test if its a baseusermapper user
+            ( $web eq $Foswiki::cfg{UsersWebName} )
+        and ( $linkText eq $topic )
+        and Foswiki::Func::wikiToUserName($topic)
+        and ( not Foswiki::Func::isGroupMember( 'BaseGroup', $topic ) )
+      )
+    {
+        $linkText =
+          AVATAR( $Foswiki::Plugins::SESSION, { _DEFAULT => $topic } )
+          . $linkText;
+    }
+
     return $linkText;
 }
-
 
 1;
 
